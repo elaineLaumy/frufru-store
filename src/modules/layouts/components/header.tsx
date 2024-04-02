582d813a07fa05c5a48c0222a4348d39a4ebf340
elaine <elaine.silva@dantaseletro.com.br>
2024-04-02 12:49:39 -0300

    feat: eu queria manda um beijo pra minha mãe, minha vó e pra você xuxa

---
+++ src/modules/layouts/components/header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <div className="flex w-full bg-rose-700 h-20">
+      <div>logo auqi</div>
+      <div>logo auqi</div> <div>logo auqi</div>
+    </div>
+  );
+}
